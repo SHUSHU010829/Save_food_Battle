d96cde4f23ffb88f67e6bd8aee52a1ed3dab869f
ShuyuanChuang <shuyuanchuang2020@gmail.com>
2022-12-05 12:06:30 +0800

    [feat] ScanPage 進度條完成

--- frontend/lib/page/navpage/scan_page.dart
+++ frontend/lib/page/navpage/scan_page.dart
@@ -1,3 +1,13 @@
+// ignore: slash_for_doc_comments
+/********************************************
+
+QRCODE掃描要有介面計數 要掃描兩個QR
+掃描區開大一點，不然很難對焦
+沒跑動android模擬器到android/app/src/build.gradle修改line 31 49 50
+只有動功能區域其他沒動
+
+********************************************/
+
 import 'dart:developer';
 import 'dart:io';
 import 'package:flutter/material.dart';
@@ -5,7 +15,7 @@ import 'package:frontend/theme/constants.dart';
 import 'package:frontend/dbHelper/user/mongodb.dart';
 import 'package:frontend/models/scanQRmodel.dart';
 import 'package:frontend/page/scanpage/edit_food_page.dart';
-import 'package:frontend/page/scanpage/stepper_widget.dart';
+import 'package:progress_stepper/progress_stepper.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:mongo_dart/mongo_dart.dart' as M;
@@ -24,16 +34,15 @@ class _ScanPageState extends State<ScanPage> {
     setState(() => showScan = !showScan);
   }
 
-  Barcode? result; //QR掃描存結果
+  Barcode? result; // QR掃描存結果
   String datainfo = "";
   int twoQRcheck = 0; //步驟偵測
-  QRViewController? controller; //控制器 不要刪
+  QRViewController? controller; // 控制器 不要刪
   final GlobalKey qrkey = GlobalKey(debugLabel: 'QR');
 
   // In order to get hot reload to work we need to pause the camera if the platform
   // is android, or resume the camera if the platform is iOS.
   @override
-  //權限控制 IOS有更新寫法到qr_code_scanner readme找
   void reassemble() {
     super.reassemble();
     if (Platform.isAndroid) {
@@ -97,106 +106,132 @@ class _ScanPageState extends State<ScanPage> {
               ],
             ),
             const SizedBox(
-              height: 40,
+              height: 16,
             ),
-            //* Qr Scanner
-            Container(
-              width: double.infinity,
-              height: 400,
-              alignment: Alignment.center,
-              child: Container(
-                width: 200,
-                height: 200,
-                color: Colors.green[300],
-                child: QRView(
-                  key: qrkey,
-                  onQRViewCreated: _onQRViewCreated,
-                  onPermissionSet: (ctrl, p) =>
-                      _onPermissionSet(context, ctrl, p),
-                ),
-                /*child: InkWell(
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const EditFoodPage(),
+            Column(
+              children: [
+                if (twoQRcheck < 2) ...[
+                  //* Qr Scanner
+                  Container(
+                    width: double.infinity,
+                    height: 400,
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: QRView(
+                        key: qrkey,
+                        onQRViewCreated: _onQRViewCreated,
+                        onPermissionSet: (ctrl, p) =>
+                            _onPermissionSet(context, ctrl, p),
+                      ),
                     ),
                   ),
-                ),*/
-              ),
-            ),
-            //* 功能按鈕
-            Container(
-              padding: const EdgeInsets.only(left: 80),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Icon(Icons.highlight),
-                        style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all(const CircleBorder()),
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.all(20)),
-                          backgroundColor: MaterialStateProperty.all(
-                              primaryColor5), // <-- Button color
-                          overlayColor:
-                              MaterialStateProperty.resolveWith<Color?>(
-                            (states) {
-                              if (states.contains(MaterialState.pressed)) {
-                                return primaryColor2;
-                              }
-                              return null; // <-- Splash color
-                            },
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 40,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
+                ] else ...[
+                  //* 確認按鈕
+                  Container(
+                    width: double.infinity,
+                    height: 400,
+                    alignment: Alignment.center,
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: () {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (BuildContext context) {
-                                // return InsertFoodPage();
+                              builder: (context) {
                                 return const EditFoodPage();
                               },
                             ),
                           );
                         },
-                        child: const Icon(Icons.edit),
-                        style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all(const CircleBorder()),
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.all(20)),
-                          backgroundColor: MaterialStateProperty.all(
-                              primaryColor5), // <-- Button color
-                          overlayColor:
-                              MaterialStateProperty.resolveWith<Color?>(
-                            (states) {
-                              if (states.contains(MaterialState.pressed)) {
-                                return primaryColor2;
-                              }
-                              return null; // <-- Splash color
-                            },
-                          ),
+                        child: const CircleAvatar(
+                          backgroundColor: primaryColor7,
+                          radius: 100,
+                          child: Text(
+                            '掃描完成！',
+                            style: TextStyle(fontSize: 25, color: Colors.white),
+                          ), //Text
                         ),
-                      ),
-                    ],
+                      ), //CircleAvatar
+                    ),
                   ),
                 ],
-              ),
+              ],
             ),
+            ProgressStepper(
+              width: 200,
+              height: 25,
+              color: secondary6,
+              progressColor: primaryColor5,
+              stepCount: 3,
+              builder: (index) {
+                double widthOfStep = 200 / 3;
+                if (index == twoQRcheck + 1) {
+                  return ProgressStepWithChevron(
+                    width: widthOfStep,
+                    defaultColor: secondary6,
+                    progressColor: primaryColor5,
+                    wasCompleted: true,
+                  );
+                }
+                return ProgressStepWithChevron(
+                  width: widthOfStep,
+                  defaultColor: secondary6,
+                  progressColor: primaryColor5,
+                  wasCompleted: false,
+                );
+              },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            header(),
           ],
         ),
       ),
     );
+  }
+
+  //-------------------------步驟提示文字
+  Widget header() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              const Icon(
+                Icons.lightbulb,
+                color: secondary5,
+                size: 32,
+              ),
+              Text(
+                headerText(),
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontFamily: chineseFontfamily,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  String headerText() {
+    switch (twoQRcheck) {
+      case 1:
+        return '先掃描右邊的 QR Code';
+      case 2:
+        return '完成！';
+      default:
+        return '先掃描左邊的 QR Code';
+    }
   }
 
   //-------------------------QRcode掃瞄功能區
@@ -253,11 +288,3 @@ class _ScanPageState extends State<ScanPage> {
     var result = await MongoDatabase.insertscan(data);
   }
 }
-/********************************************
-
-QRCODE掃描要有介面計數 要掃描兩個QR
-掃描區開大一點，不然很難對焦
-沒跑動android模擬器到android/app/src/build.gradle修改line 31 49 50
-只有動功能區域其他沒動
-
-********************************************/
