6fee100dd8506ff77bcce3284c54ba5be64c8ede
ShuyuanChuang <shuyuanchuang2020@gmail.com>
2022-12-06 02:00:08 +0800

    [feat] 增加app載入畫面 [fix] 修改 app icon

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:frontend/constants.dart';
+import 'package:frontend/size_config.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -10,7 +12,7 @@ import 'package:frontend/page/navpage/main_page.dart'; // ignore: unused_import
 import 'package:frontend/page/storefood/foodDetail/food_detail_observer.dart';
 import 'page/openScreen/onboarding_screen.dart'; // ignore: unused_import
 import 'package:bloc/bloc.dart'; // ignore: unused_import
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 // final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 //     FlutterLocalNotificationsPlugin();
 
@@ -41,6 +43,7 @@ void main() async {
   Bloc.observer = FoodDetailObserver();
   await MongoDatabase.connect();
 
+  // ------------------------ 本地通知設定
   // AndroidInitializationSettings androidSettings =
   //     const AndroidInitializationSettings("@mipmap/ic_launcher");
 
@@ -68,13 +71,68 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Food_Battle_App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'GenJyuuGothic',
+    return FutureBuilder(
+      future: Future.delayed(const Duration(seconds: 3)),
+      builder: (context, AsyncSnapshot snapshot) {
+        // Loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            title: 'Food_Battle_App',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              fontFamily: 'GenJyuuGothic',
+            ),
+            home: const SplashPage(),
+          );
+        }
+
+        // Main
+        else {
+          return MaterialApp(
+            title: 'Food_Battle_App',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              fontFamily: 'GenJyuuGothic',
+            ),
+            home: const OnboardingScreen(),
+          );
+        }
+      },
+    );
+  }
+}
+
+// SplashPage
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/icons/app_icon.png",
+              height: SizeConfig.blockV! * 23,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "拯救食物大作戰",
+              style: TextStyle(
+                color: primaryColor7,
+                fontFamily: chineseFontfamily,
+                fontSize: 24,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ],
+        ),
       ),
-      home: const OnboardingScreen(),
     );
   }
 }
